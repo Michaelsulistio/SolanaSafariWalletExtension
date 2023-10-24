eb1d52e5e9c698690fe1688a050411881904c50a
michael sulistio <michaelsulistio@gmail.com>
2023-10-24 16:21:43 -0700

    Cleanup and refactor background.ts

--- js-extension/src/background.ts
+++ js-extension/src/background.ts
@@ -9,73 +9,58 @@ Script that makes up the extension's background page.
 
 import { BaseWalletRequest } from "./types/messageTypes";
 
-async function initializeApprovalTab_noRaceCondition(): Promise<browser.tabs.Tab> {
-  const getReadyTabPromise = new Promise<browser.tabs.Tab>(
-    (resolve, reject) => {
-      const onApproveTabReady = (
-        _tabId: number,
-        changeInfo: browser.tabs._OnUpdatedChangeInfo,
-        tab: browser.tabs.Tab
-      ) => {
-        if (
-          tab.url === browser.runtime.getURL("approval.html") &&
-          changeInfo.status === "complete"
-        ) {
-          browser.tabs.onUpdated.removeListener(onApproveTabReady);
-          resolve(tab);
-        }
-      };
-      browser.tabs.onUpdated.addListener(onApproveTabReady);
-    }
-  );
+async function initializeApprovalTab(): Promise<browser.tabs.Tab> {
+  return new Promise<browser.tabs.Tab>((resolve, reject) => {
+    const onApproveTabReady = (
+      _tabId: number,
+      changeInfo: browser.tabs._OnUpdatedChangeInfo,
+      tab: browser.tabs.Tab
+    ) => {
+      if (
+        tab.url === browser.runtime.getURL("approval.html") &&
+        changeInfo.status === "complete"
+      ) {
+        browser.tabs.onUpdated.removeListener(onApproveTabReady);
+        resolve(tab);
+      }
+    };
 
-  browser.tabs.create({
-    url: browser.runtime.getURL("approval.html")
+    browser.tabs.onUpdated.addListener(onApproveTabReady);
+
+    browser.tabs
+      .create({
+        url: browser.runtime.getURL("approval.html")
+      })
+      .catch((error) => {
+        browser.tabs.onUpdated.removeListener(onApproveTabReady);
+        reject(error);
+      });
   });
-
-  return getReadyTabPromise;
 }
 
 async function forwardWalletRequestToApproval(request: BaseWalletRequest) {
-  const isApprovalUIActive = await browser.tabs
-    .query({ active: true, currentWindow: true })
-    .then((tabs) => {
-      const activeTab = tabs[0];
-      return browser.runtime.getURL("approval.html") === activeTab.url;
-    });
+  const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  const activeTab = tabs[0];
+  const isApprovalUIActive =
+    browser.runtime.getURL("approval.html") === activeTab.url;
 
-  if (isApprovalUIActive) {
-    console.log("Approval UI is Active");
-    // Forward to approval UI
-  } else {
-    console.log("Approval UI is not Active");
-    // Initialize and forward
-    initializeApprovalTab_noRaceCondition().then((approveTab) => {
-      if (approveTab.id) {
-        const newreq = {
-          ...request,
-          type: "approval-tab-request"
-        };
-        console.log("Right before sending: ", newreq);
-        browser.tabs.sendMessage(approveTab.id, {
-          ...request,
-          type: "approval-tab-request"
-        });
-      } else {
-        console.error("Approval tab missing id");
-      }
+  const targetTab = isApprovalUIActive
+    ? activeTab
+    : await initializeApprovalTab();
+
+  if (targetTab.id) {
+    browser.tabs.sendMessage(targetTab.id, {
+      ...request,
+      type: "approval-tab-request"
     });
+  } else {
+    console.error("Approval tab is missing tab id");
   }
 }
 
 browser.runtime.onMessage.addListener(
-  async (request, sender: browser.runtime.MessageSender, sendResponse) => {
-    console.log("Background request received: ", request);
-    console.log("From Sender: ", sender);
-    console.log(sendResponse);
-    if (request.type === "wallet-response") {
-      // forwardApprovalResponseToContent(request as WalletResponse);
-    } else if (request.type === "wallet-approval-request") {
+  async (request, sender: browser.runtime.MessageSender, _sendResponse) => {
+    if (request.type === "wallet-approval-request") {
       // Attach sender identity metadata before forwarding
       forwardWalletRequestToApproval({
         ...request,
