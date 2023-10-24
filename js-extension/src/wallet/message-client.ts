9f7a1a5113348344fa307814c122773757ba2df6
michael sulistio <michaelsulistio@gmail.com>
2023-10-24 16:24:54 -0700

    Cleanup message-client

--- js-extension/src/wallet/message-client.ts
+++ js-extension/src/wallet/message-client.ts
@@ -24,27 +24,19 @@ export default class MessageClient {
   } = {};
 
   constructor() {
-    // Global listener for content responses
     window.addEventListener("wallet-response", this.#handleResponse.bind(this));
   }
 
-  // Handles responses from content script
+  // Handles wallet responses from content script.
   #handleResponse(event: Event) {
-    console.log("In MessageClient wallet handle response: ", event);
     const response = (event as WalletResponseEvent).detail;
     const requestId = response?.requestId;
 
     if (requestId && this.#resolveHandler[requestId]) {
-      console.log("Handler for wallet response: ", event);
       const { resolve, reject } = this.#resolveHandler[requestId];
-
-      if (true) {
-        console.log("Resolving request: ", requestId);
-        const decodedResponse = decodeWalletResponse(response);
-        resolve(decodedResponse);
-      }
-
-      delete this.#resolveHandler[requestId]; // Cleanup
+      const decodedResponse = decodeWalletResponse(response);
+      resolve(decodedResponse);
+      delete this.#resolveHandler[requestId];
     }
   }
 
@@ -68,7 +60,6 @@ export default class MessageClient {
       );
 
       this.#resolveHandler[request.requestId] = { resolve, reject };
-      console.log("Sending request: ", request.requestId);
       window.dispatchEvent(walletRequest);
     });
   }
